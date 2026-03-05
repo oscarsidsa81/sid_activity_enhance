5381189412a46d714e9df76a44a442189787fa31
oscarsidsa81 <ovilches@sid-sa.com>
2026-03-05 09:48:25 +0100

    Commit

--- __manifest__.py
+++ __manifest__.py
@@ -1,5 +1,5 @@
 {
-    'name': 'SID - Sale Activity (OV)',
+    'name': 'sid_activity_enhance',
     'version': '15.0.1.0.1',
     'category': 'Sales',
     'summary': 'Personalizaciones de sale.activity (campos, vistas y lógicas) trasladadas a módulo.',
@@ -17,7 +17,6 @@
         'views/sale_activity_views.xml',
         'views/sale_activity_tag_rule_views.xml',
         'views/sale_line_activity_wizard_views.xml',
-        'views/sale_line_activity_integration_views.xml',
     ],
     'post_init_hook': 'post_init_hook',
     'installable': True,
