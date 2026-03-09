8e7f77b0fcafbefb4e72146f2b003596a60d039d
oscarsidsa81 <ovilches@sid-sa.com>
2026-03-09 14:01:18 +0100

    Commit

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,5 @@
 from . import sale_activity
 from . import activity_tag
 from . import activity_tag_rule
-
 from . import sale_order_line
-from . import sale_order
 from . import stock_move
-from . import stock_picking
